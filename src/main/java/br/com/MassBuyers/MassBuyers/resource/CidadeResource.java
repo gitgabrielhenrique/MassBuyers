398d5516ed471d63c8b619e7c45e5ccb670373c6
gitgabrielhenrique <gabrielHsantos2018@gmail.com>
2023-10-30 11:13:34 -0300

    ihuuuuuuuuuuuuuu

--- src/main/java/br/com/MassBuyers/MassBuyers/resource/CidadeResource.java
+++ src/main/java/br/com/MassBuyers/MassBuyers/resource/CidadeResource.java
@@ -9,6 +9,7 @@ import br.com.MassBuyers.MassBuyers.service.CidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +24,7 @@ public class CidadeResource {
 
   @Autowired
   private CidadeRepository cidadeRepository;
-
+private CidadeService cidadeService;
 
   @GetMapping("/todos")
   public List<Cidade> ListarTodasAsCidades(){
@@ -35,9 +36,25 @@ return cidadeRepository.findAll();
     return  cidadeRepository.Filtrar(cidadefilter,pageable);
   }
 //continua aqui viado
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void remover(@PathVariable Long id){cidadeRepository.deleteById(id);}
+
+
+
+  @PutMapping("/{id}")
+  public  ResponseEntity<Cidade> atualizar(@PathVariable Long id,@Valid @RequestBody Cidade cidade ){
+    Cidade cidadesalva=cidadeService.atualizar(id,cidade);
+    return  ResponseEntity.ok(cidadesalva);
+  }
+
+
+
+
   @PostMapping()
   public ResponseEntity<Cidade> criar(@Valid @RequestBody Cidade cidade, HttpServletResponse response){
-     ;
+     Cidade cidadesalva= cidadeService.salvar(cidade);
+     return ResponseEntity.status(HttpStatus.CREATED).body(cidadesalva);
 
   }
 
